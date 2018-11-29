375c730877d86c0e83e6e73851231e9da4b68e29
Lucas Nunes <nunesmlucasg@gmail.com>
2018-11-29 18:35:22 -0500

    number incremental and hangman in js adjustment.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,9 @@
-//When I put this section into my <script> area in my index.html it will work. 
- var userText = document.getElementById("user-text");
- userText.textContent = htest;
- //Instead it can't find/see the ID: "user-text" from the index.html file 
- // and returns null... 
-//------------------------------------------------------------------
 
+window.addEventListener("load", function (event) {
+    console.log("All resources finished loading!");
+    var userText = document.getElementById("user-text");
+    userText.textContent = htest;
+});
 
 var artistChoices = ["Coldplay", "Madonna", "Dave Matthews Band", "Mumford and Sons", "Logic", "Conro"];
 console.log(artistChoices);
@@ -37,9 +36,12 @@ console.log("HANGJOIN VAR: " + hangJoin);
 var htest = hangJoin.toString();
 console.log("HANG TEST STRING: " + htest);
 
+//Increases Wins value by 1. 
+document.onkeyup = function () {
+    console.log(event);
+    winsAdded++;
+    console.log(winsAdded);
+    var winsScore = document.getElementById("wins");
+    winsScore.textContent = winsAdded;
+};
 
-// document.onkeyup = function(){
-//     console.log(event);
-//     winsAdded++;
-//     console.log(winsAdded);
-//   };
