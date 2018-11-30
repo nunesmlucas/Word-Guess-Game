241b8dfab630a8d8dc84380b06aac1d3fe9f87b1
Lucas Nunes <nunesmlucasg@gmail.com>
2018-11-30 13:16:44 -0500

    guessedLetters 1.0

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@
 window.addEventListener("load", function (event) {
     console.log("All resources finished loading!");
     var userText = document.getElementById("user-text");
-    userText.textContent = htest;
+    userText.textContent = hangJoin;
 });
 
 var artistChoices = ["Coldplay", "Madonna", "Dave Matthews Band", "Mumford and Sons", "Logic", "Conro"];
@@ -18,11 +18,10 @@ var randLetters = randChoice.split("");
 console.log("RANDOM LETTERS: " + randLetters);
 
 
-
 var hangHold = [];
 for (var i = 0; i < randLetters.length; i++) {
     if (randLetters[i] === " ") {
-        hangHold.push(" | ");
+        hangHold.push("  ");
     }
     else {
         hangHold.push(" _ ");
@@ -32,16 +31,51 @@ for (var i = 0; i < randLetters.length; i++) {
 console.log(hangHold);
 console.log("HANGHOLD JOIN HERE: " + hangHold.join(""));
 var hangJoin = hangHold.join("");
-console.log("HANGJOIN VAR: " + hangJoin);
-var htest = hangJoin.toString();
-console.log("HANG TEST STRING: " + htest);
 
 //Increases Wins value by 1. 
-document.onkeyup = function () {
+// document.onkeyup = function () {
+//     console.log(event);
+//     winsAdded++;
+//     console.log(winsAdded);
+//     var winsScore = document.getElementById("wins");
+//     winsScore.textContent = winsAdded;
+// };
+
+var letters = /^[A-Za-z]+$/;
+var letterArray = [];
+var lettersGuessed;
+var keyHolder;
+
+
+// document.onkeyup = function () {
+// if (e.keyCode == 13) {
+document.onkeydown = function (event) {
     console.log(event);
-    winsAdded++;
-    console.log(winsAdded);
-    var winsScore = document.getElementById("wins");
-    winsScore.textContent = winsAdded;
+    keyholder = event.key;
+    if (keyholder.match(letters)) {
+        letterArray.push("  " + keyholder);
+    }
+    else {
+        alert("Please enter letters only!");
+    }
+    // letterArray.push("  " + event.key);
+    console.log(letterArray);
+    lettersGuessed = document.getElementById("guessed-letters");
+    lettersGuessed.textContent = letterArray.toString().toUpperCase();
+
+    for(var l =0; l<randLetters.length; l++){
+        if(randLetters[l].match(keyholder)){
+            hangHold[l] == (keyholder);
+            console.log("RANDLETTER[l]:--> "+randLetters[l] + " -- KEYHOLDER --> "+ keyholder);
+            console.log("HANGHOLD[l]: --->" + hangHold[l]);
+            hangHold[l] == "G";
+            console.log(hangHold[l]);
+        }
+    }
 };
+
+    // }
+// }
+
+
 
